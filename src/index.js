effd253e679339346c79b1d06bf57d0302fa9cb8
ariel taylor <ariel.taylorr@gmail.com>
2021-02-09 16:16:38 -0800

    Click adds a pattern and clears the canvas

--- src/index.js
+++ src/index.js
@@ -4,33 +4,36 @@ import Circle from "./scripts/circles";
 import Pattern from './scripts/pattern_option1';
 
 
-
-
-
+document.querySelector("#pattern1").addEventListener("click", drawPattern)
+document.querySelector("#pattern2").addEventListener("click", drawPattern)
+document.querySelector("#pattern3").addEventListener("click", drawPattern)
 
 function drawPattern(){
+    debugger
+    clearCanvas();
     const canvas = new Canvas();
-    canvas.createCanvas();
-    const source = '../../dist/images/ab_mono_rb.png';
-    const pattern = new Pattern(canvas.ctx, source);
+    let id;
+    if (this.id === "pattern1"){
+        id = '../../dist/images/ab_mono_rb.png'
+    } else if (this.id === "pattern2"){
+        id = '../../dist/images/ab_flowers.png';
+    } else {
+        id = '../../dist/images/ab_stripe.png';
+    }
+    const pattern = new Pattern(canvas.ctx, id);
     pattern.placeImage();
     
 
 }
-drawPattern();
 
 
 function startCircles(){
     const canvas = new Canvas();
-    canvas.createCanvas();
-    debugger
-
     let circleArray = [];
 
 for (let i = 0; i < 10; i ++) {
     let radius = 40;
     let x = Math.random() * (canvas.canvas.width - radius * 2) + radius;
-    debugger
     let y = Math.random() * (canvas.canvas.height - radius * 2) + radius;
     let dy = (Math.random() -.5);
     let dx = (Math.random() -.5);
@@ -42,7 +45,7 @@ console.log(circleArray)
 
 function animateCircles(){
     requestAnimationFrame(animateCircles);
-    canvas.ctx.clearRect(0, 0, canvas.canvas.width, canvas.canvas.height)
+    canvas.ctx.clearRect(0, 0, canvas.canvas.width, canvas.canvas.height);
 
     for (let i = 0; i < circleArray.length; i ++){
         circleArray[i].update()
@@ -53,3 +56,11 @@ animateCircles();
 }
 
 // startCircles();
+
+function clearCanvas(){
+    debugger
+    document.querySelector("canvas") ? 
+    document.getElementById("canvas-container").removeChild(document.querySelector("canvas"))
+    : null;
+    
+}
--- src/scripts/canvas.js
+++ src/scripts/canvas.js
@@ -3,53 +3,14 @@ class Canvas {
     constructor(props) {
         this.canvasContainer = document.getElementById("canvas-container");
         this.canvas = document.createElement("canvas");
-        // debugger
         this.canvas.width = window.innerWidth * .50;
-        // debugger
         this.canvas.height = this.canvas.width/ 1.46;
         this.ctx = this.canvas.getContext("2d");
-    }
-
-    createCanvas() {
         this.canvasContainer.appendChild(this.canvas);
     }
+
+    // createCanvas() {
+    //     this.canvasContainer.appendChild(this.canvas);
+    // }
 }
 export default Canvas;
-
-// let canvas = document.getElementById("canvas");
-// canvas.width = window.innerWidth * .50;
-// canvas.height = canvas.width / 1.46;
-// let ctx = canvas.getContext('2d');
-// const person = document.getElementById("person");
-// canvas.drawImage(person, 10, 10);
-
-// ctx.fillStyle = '#A1C0BA';
-// ctx.fillRect(10, 10, 300, 300)
-
-// //LINE
-
-// ctx.beginPath();
-// ctx.moveTo(50, 300);
-// ctx.lineTo(300, 100);
-// ctx.lineTo(400, 500);
-// ctx.strokeStyle = "#CF8562";
-// ctx.stroke();
-
-// arc 
-// ctx.beginPath();
-// ctx.arc(300, 330, 30, 0, Math.PI * 2, false);
-// ctx.stroke();
-
-// for ( let i = 0; i < 100; i ++ ){
-//     let x = Math.random() * canvas.width;
-//     let y = Math.random() * canvas.height;
-//     ctx.beginPath();
-//     ctx.arc(x, y, 30, 0, Math.PI * 2, false);
-//     ctx.stroke();
-// }
-
-
-
-
-
-
